e1e1d899cf8ffa5703196dde1054dccd13c9927a
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-04-18 09:31:32 +0530

    browse

--- Frontend/src/components/ManageUsers.jsx
+++ Frontend/src/components/ManageUsers.jsx
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from 'react'
+import React,{useState, useEffect} from 'react'
 
 const ManageUsers = () => {
     const [Data, setData] = useState([])
 
     const fetchUserData = async () => {
-        const res = await fetch('http://localhost:5000/user/getall',)
-        // console.log(res.status)
-        if(res.status === 200){
-            const data = await res.json()
+        const res = await fetch('http://localhost:3000/user/getall')
+        console.log(res.status)
+        if (res.status === 200){
+            const data = await res.json();
             console.log(data)
             setData(data)
         }
@@ -16,36 +16,46 @@ const ManageUsers = () => {
         fetchUserData()
     }, [])
 
-    const displayUsers = () => {
-        return Data.map((user) => {
+    const deleteProduct = async (id) => {
+        console.log(id);
+        const res = await fetch("http://localhost:3000/user/delete/" + id, {
+            method: "DELETE",
+        });
+        if (res.status === 200) {
+            fetchUserData();
+        }
+    }
+
+    const displayUser = () =>{
+        return Data.map((user) => (
             <tr>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                    <button className='btn btn-danger'>Delete</button>
+                    <button onClick = {() => { deleteProduct(user._id)}} className="btn btn-danger">Delete</button>
                 </td>
             </tr>
-        })
+        ))
     }
-
   return (
-    <div className=''>
-        <header className='bg-danger mt-5 text-white text-center'>
+    <div>
+        <header className='bg-danger text-white'>
             <div className="container py-5">
-                <h1>Manage Users</h1>
+                <h1>Manage User</h1>
             </div>
         </header>
 
-        <div className="container mt-3">
-            <table className='table'>
+        <div className="container mt-5">
+            <table className="table table-dark">
                 <thead>
                     <tr>
                         <th>Name</th>
                         <th>Email</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
-                    {displayUsers()}
+                    {displayUser()}
                 </tbody>
             </table>
         </div>
